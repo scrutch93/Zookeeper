2e4c345750a256c66cd9b7bfe693c2f95b376b08
Steven <stevccru@icloud.com>
2022-02-17 13:36:31 -0500

    Initial commit

---
+++ Topics/Standard output/Correct the code/src/Main.kt
@@ -0,0 +1,7 @@
+fun main() {
+    System.out.printf(1, 2, 3...)
+    printLine("Line print here ")
+    lnPrint(42)
+    printLn("Pay attention to")
+    println("syntax")
+}
---
+++ Topics/Standard output/Print code/src/Main.kt
@@ -0,0 +1,7 @@
+fun main() {
+    // only println are needed here...
+    val one = 1
+    val two = 2
+    println()// empty println
+
+}
